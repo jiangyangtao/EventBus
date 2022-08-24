624052bea21f22633c7b2c3c9d17af1f3fcb065c
Yangtao Jiang <jiangyangtao1@qq.com>
2022-08-24 18:16:18 +0800

    Update ISubscription

--- src/EventBus.Abstractions/IModels/ISubscription.cs
+++ src/EventBus.Abstractions/IModels/ISubscription.cs
@@ -8,22 +8,9 @@ namespace EventBus.Abstractions.IModels
     public interface ISubscription : IBaseModel
     {
         /// <summary>
-        /// 事件 Id
-        /// </summary>
-        public Guid EvnetId { get;  }
-
-
-        /// <summary>
         /// 事件
         /// </summary>
         public IEvent Event { get;  }
-
-
-        /// <summary>
-        /// 应用订阅的接入点 Id
-        /// </summary>
-        public Guid ApplicationEndpointId { set; }
-
 
         /// <summary>
         /// 应用订阅的接入点
