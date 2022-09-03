b1b1f81e429ec9ff78b7b72b53c4a97861f0abb9
Yangtao Jiang <jiangyangtao1@qq.com>
2022-09-04 07:10:13 +0800

    Add Database context the dynamic entity

--- src/EventBus.Storage.Core/EventBusDBContext.cs
+++ src/EventBus.Storage.Core/EventBusDBContext.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EventBus.Storage.Abstractions.IRepositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace EventBus.Storage.Core
 {
@@ -15,6 +11,14 @@ namespace EventBus.Storage.Core
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes().Where(t => t.GetInterfaces().Contains(typeof(IEntity)))).ToArray();
+            foreach (var v in types)
+            {
+                if (v.IsAbstract) continue;
+
+                modelBuilder.Entity(v);
+            }
+
             base.OnModelCreating(modelBuilder);
         }
     }
