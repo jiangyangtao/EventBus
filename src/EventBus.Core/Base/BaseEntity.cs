3111c9fd99a967444145afa04bffe541785a71d7
Yangtao Jiang <jiangyangtao1@qq.com>
2022-10-04 03:04:21 +0800

    Add SequentialGuid

--- src/EventBus.Core/Base/BaseEntity.cs
+++ src/EventBus.Core/Base/BaseEntity.cs
@@ -2,14 +2,58 @@
 using EventBus.Storage.Abstractions.IRepositories;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography;
 
 namespace EventBus.Core.Base
 {
     public abstract class BaseEntity<TEntity> : IBaseModel, IEntity
     {
+        private static readonly RandomNumberGenerator RandomNumberGenerator = RandomNumberGenerator.Create();
+
         public BaseEntity()
         {
-            Id = Guid.NewGuid();
+            Id = Create(SequentialGuidType.SequentialAsString);
+        }
+
+        private static Guid Create(SequentialGuidType guidType)
+        {
+            var randomBytes = new byte[10];
+            RandomNumberGenerator.GetBytes(randomBytes);
+
+            var timestamp = DateTime.UtcNow.Ticks / 10000L;
+            var timestampBytes = BitConverter.GetBytes(timestamp);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(timestampBytes);
+            }
+
+            var guidBytes = new byte[16];
+
+            switch (guidType)
+            {
+                case SequentialGuidType.SequentialAsString:
+                case SequentialGuidType.SequentialAsBinary:
+
+                    Buffer.BlockCopy(timestampBytes, 2, guidBytes, 0, 6);
+                    Buffer.BlockCopy(randomBytes, 0, guidBytes, 6, 10);
+
+                    if (guidType == SequentialGuidType.SequentialAsString && BitConverter.IsLittleEndian)
+                    {
+                        Array.Reverse(guidBytes, 0, 4);
+                        Array.Reverse(guidBytes, 4, 2);
+                    }
+
+                    break;
+
+                case SequentialGuidType.SequentialAtEnd:
+
+                    Buffer.BlockCopy(randomBytes, 0, guidBytes, 0, 10);
+                    Buffer.BlockCopy(timestampBytes, 2, guidBytes, 10, 6);
+                    break;
+            }
+
+            return new Guid(guidBytes);
         }
 
         [Key]
@@ -21,5 +65,24 @@ namespace EventBus.Core.Base
 
         [NotMapped]
         public static TEntity[] EmptyArray => Array.Empty<TEntity>();
+    }
+
+    /// <summary>
+    /// Sequential Guid Type
+    /// </summary>
+    internal enum SequentialGuidType
+    {
+        /// <summary>
+        /// Sequential As String
+        /// </summary>
+        SequentialAsString,
+        /// <summary>
+        /// Sequential As Binary
+        /// </summary>
+        SequentialAsBinary,
+        /// <summary>
+        /// Sequential A tEnd
+        /// </summary>
+        SequentialAtEnd
     }
 }
